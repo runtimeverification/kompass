3af034567b2b6b047b81f2d359f2fbd73095e699
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2025-05-08 18:51:36 -0500

    Add prove raw and prune

--- src/kompass/__main__.py
+++ src/kompass/__main__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import os
 import sys
 from pathlib import Path
@@ -7,23 +8,30 @@ from typing import TYPE_CHECKING
 
 from kmir.__main__ import _arg_parser, _parse_args
 from kmir.cargo import CargoProject
-from kmir.options import ProveRSOpts, RunOpts, ShowOpts, ViewOpts
+from kmir.options import ProveRawOpts, ProveRSOpts, PruneOpts, RunOpts, ShowOpts, ViewOpts
 from kmir.parse.parser import parse_json
 from kmir.smir import SMIRInfo
-from pyk.proof.reachability import APRProof
+from pyk.proof.reachability import APRProof, APRProver
 from pyk.proof.show import APRProofShow
 from pyk.proof.tui import APRProofViewer
 
 from kompass.kompass import HASKELL_DEF_DIR, LLVM_DEF_DIR, LLVM_LIB_DIR, Kompass, KompassAPRNodePrinter
 
 if TYPE_CHECKING:
+    from argparse import Namespace
     from collections.abc import Sequence
+    from typing import Final
+
+
+_LOGGER: Final = logging.getLogger(__name__)
+_LOG_FORMAT: Final = '%(levelname)s %(asctime)s %(name)s - %(message)s'
 
 
 def kompass(args: Sequence[str]) -> None:
     parser = _arg_parser()
     parser.prog = 'kompass'
     ns, remaining = parser.parse_known_args(args)
+    logging.basicConfig(level=_loglevel(ns), format=_LOG_FORMAT)
 
     opts = _parse_args(ns)
     match opts:
@@ -31,10 +39,14 @@ def kompass(args: Sequence[str]) -> None:
             _kompass_run(opts)
         case ProveRSOpts():
             _kompass_prove_rs(opts)
+        case ProveRawOpts():
+            _kompass_prove_raw(opts)
         case ShowOpts():
             _kompass_show(opts)
         case ViewOpts():
             _kompass_view(opts)
+        case PruneOpts():
+            _kompass_prune(opts)
         case _:
             raise AssertionError
 
@@ -68,6 +80,26 @@ def _kompass_prove_rs(opts: ProveRSOpts) -> None:
         sys.exit(1)
 
 
+def _kompass_prove_raw(opts: ProveRawOpts) -> None:
+    kompass = Kompass(HASKELL_DEF_DIR, LLVM_LIB_DIR, bug_report=opts.bug_report)
+    claim_index = kompass.get_claim_index(opts.spec_file)
+    labels = claim_index.labels(include=opts.include_labels, exclude=opts.exclude_labels)
+    for label in labels:
+        print(f'Proving {label}')
+        claim = claim_index[label]
+        if not opts.reload and opts.proof_dir is not None and APRProof.proof_data_exists(label, opts.proof_dir):
+            _LOGGER.info(f'Reading proof from disc: {opts.proof_dir}, {label}')
+            proof = APRProof.read_proof_data(opts.proof_dir, label)
+        else:
+            _LOGGER.info(f'Constructing initial proof: {label}')
+            proof = APRProof.from_claim(kompass.definition, claim, {}, proof_dir=opts.proof_dir)
+        with kompass.kcfg_explore(label) as kcfg_explore:
+            prover = APRProver(kcfg_explore, execute_depth=opts.max_depth)
+            prover.advance_proof(proof, max_iterations=opts.max_iterations)
+        summary = proof.summary
+        print(f'{summary}')
+
+
 def _kompass_show(opts: ShowOpts) -> None:
     kompass = Kompass(HASKELL_DEF_DIR, LLVM_LIB_DIR)
     proof = APRProof.read_proof_data(opts.proof_dir, opts.id)
@@ -91,6 +123,13 @@ def _kompass_view(opts: ViewOpts) -> None:
     viewer.run()
 
 
+def _kompass_prune(opts: PruneOpts) -> None:
+    proof = APRProof.read_proof_data(opts.proof_dir, opts.id)
+    pruned_nodes = proof.prune(opts.node_id)
+    print(f'Pruned nodes: {pruned_nodes}')
+    proof.write_proof_data()
+
+
 def _run_build(args: Sequence[str]) -> None:
     stable_mir_path = Path.home() / '.stable-mir-json'
     if not stable_mir_path.is_dir():
@@ -105,6 +144,14 @@ def _run_build(args: Sequence[str]) -> None:
     os.environ['RUSTC'] = str(stable_mir_script)
 
     os.execvpe('cargo', ['cargo', 'build', *args], os.environ)
+
+
+def _loglevel(args: Namespace) -> int:
+    if args.debug:
+        return logging.DEBUG
+    if args.verbose:
+        return logging.INFO
+    return logging.WARNING
 
 
 def main() -> None:
